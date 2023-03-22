dccdc333978e41db16f72c364b870a8793af6155
Mycroft <kaseyfitton@hotmail.co.uk>
2023-03-22 13:01:01 -0700

    feat: Dildo Prop

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,6 +6,11 @@ lua54 'yes'
 author 'Mycroft'
 description 'Harry Potter Wand in FiveM!'
 
+files {
+    "stream/*.*"
+}
+data_file "DLC_ITYP_REQUEST" "stream/*.ytyp"
+this_is_a_map 'yes'
 shared_script 'config.lua'
 client_scripts {
     'client.lua'
